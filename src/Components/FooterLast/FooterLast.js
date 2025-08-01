67037f4d2bb334733fd4682dd97ddcdf2353f580
prabhat2580 <prabhatsingh258025@gmail.com>
2025-08-01 18:04:33 +0530

    herf

--- src/Components/FooterLast/FooterLast.js
+++ src/Components/FooterLast/FooterLast.js
@@ -31,15 +31,29 @@ export default function FooterLast() {
 <div className="base-content">
   <h3>For better experience, download the Swiggy app now</h3>
   <div className="store-buttons">
+  <a
+    href="https://apps.apple.com/in/app/swiggy-food-instamart-dineout/id989540920?referrer=utm_source%3Dswiggy%26utm_medium%3Dhomepage"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
     <img
       src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-AppStore_lg30tv"
       alt="App Store"
     />
+  </a>
+
+  <a
+    href="https://play.google.com/store/apps/details?id=in.swiggy.android&pli=1"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
     <img
       src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-GooglePlay_1_zixjxl"
       alt="Google Play"
     />
-  </div>
+  </a>
+</div>
+
 </div>
 
     </div>
