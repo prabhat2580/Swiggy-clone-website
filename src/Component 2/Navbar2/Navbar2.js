c9aac01229b699952aaf3abd05778df296aa436f
prabhat2580 <prabhatsingh258025@gmail.com>
2025-08-07 17:53:25 +0530

    add responsivenes of navbar

--- src/Component 2/Navbar2/Navbar2.js
+++ src/Component 2/Navbar2/Navbar2.js
@@ -1,28 +1,42 @@
-import React from 'react'
-import './Navbar2.css'
-import { Link } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import './Navbar2.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar2() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <div className='Navbar2-container'>
-        <div className='swiggy-logo-other'>
-            <img src="./images/swiggy12.png" alt="" />
-            <button>Others </button>
-            
-        </div>
+      <div className='swiggy-logo-other'>
+        <img src="./images/swiggy12.png" alt="Swiggy" />
+        <button>Others</button>
+      </div>
 
-        <div className='nav-items-list'><ul>
-          <a href="https://www.swiggy.com/corporate/" style={{textDecoration:'none'}}> <li> Swiggy Corporate</li></a> 
-            <li>Search</li>
-            <li>Offers</li>
-            <li>Help</li>
-            <li><Link to="/cart" style={{ textDecoration: 'none', color: 'inherit' }}>
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <div className={`nav-items-list ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li>
+            <a
+              href="https://www.swiggy.com/corporate/"
+              style={{ textDecoration: 'none', color: 'inherit' }}
+            >
+              Swiggy Corporate
+            </a>
+          </li>
+          <li>Search</li>
+          <li>Offers</li>
+          <li>Help</li>
+          <li>
+            <Link to="/cart" style={{ textDecoration: 'none', color: 'inherit' }}>
               Cart
             </Link>
-            </li>
-            
-            </ul>
-            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
